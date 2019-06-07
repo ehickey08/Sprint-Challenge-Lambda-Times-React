123a5649de49234bc2613e597c9b8d5b05d06596
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-06-07 12:37:47 -0400

    Display tabs and cards; use styled components for all css

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
+const CardDiv = styled.div`
+    border-bottom: 1px solid lightgrey;
+    display: flex;
+    justify-content: space-between;
+    align-items: none;
+    flex-direction: column;
+    background-color: #fff;
+    width: 380px;
+    margin-bottom: 16px;
+    padding: 24px;
+`
+const CardHeadline = styled.div`
+    font-size: 25px;
+    font-family: Didot, serif;
+`
+
+const CardAuthor = styled.div`
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 15px;
+`
+
+const CardImg = styled.div`
+    padding-right: 10px;
+    border-right: 1px solid lightgrey;
+    height: 40px;
+
+    img{
+        width: 40px;
+    }
+`
+
+const AuthorName = styled.span`
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+`
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
+    <CardDiv>
+      <CardHeadline>{props.card.headline}</CardHeadline>
+      <CardAuthor>
+        <CardImg>
+          <img src={`${props.card.img}`} />
+        </CardImg>
+        <AuthorName>By {props.card.author}</AuthorName>
+      </CardAuthor>
+    </CardDiv>
   );
 };
 
-// Make sure to include PropTypes.
+Card.propTypes ={
+    card: PropTypes.shape({
+        headline: PropTypes.string,
+        tab: PropTypes.string,
+        img: PropTypes.string,
+        author: PropTypes.string,
+    }),
+}
 
 export default Card;
